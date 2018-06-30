1a1260a94b5d62894f3e36b43877776786211e4b
protatodev <thaddonaghue@yahoo.com>
2018-06-29 19:25:24 -0700

    Add project files.

---
+++ friend_letter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace friend_letter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
