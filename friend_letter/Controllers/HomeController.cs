97d74d2809508bd8a80861bb06229b4ea2b5bea5
protatodev <thaddonaghue@yahoo.com>
2018-06-29 20:35:02 -0700

    add controller and view for making a basic html letter

---
+++ friend_letter/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FriendLetter.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/hello")]
+        public string Hello()
+        {
+            return "Hello friend!";
+        }
+
+        [Route("/goodbye")]
+        public string Goodbye()
+        {
+            return "Goodbye friend.";
+        }
+
+        [Route("/")]
+        public ActionResult Letter()
+        {
+            return View();
+        }
+    }
+}
